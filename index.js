34b172260989c6ecc1f8896ce5d6bcf7728a8a41
Jawad sabbah <sabbah.m.jawad@gmail.com>
2025-07-26 11:15:55 +0300

    finish the login-register and homelanding page

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.set('view engine', 'ejs');
 
 
 app.get("/",(req,res)=>{
-  res.render("auth/register")
+  res.render("home")
 })
+
+
 
 app.listen(port,()=>{
   console.log(`server start at localhost:${port}`);
