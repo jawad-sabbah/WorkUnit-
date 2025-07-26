a65cd7cf9ffd5b4ae74ece1cafb6f59b2d35d13b
Jawad sabbah <sabbah.m.jawad@gmail.com>
2025-07-26 11:09:57 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+
+const app=express();
+const port=3000;
+
+
+app.set('view engine', 'ejs'); 
+
+
+app.get("/",(req,res)=>{
+  res.render("auth/register")
+})
+
+app.listen(port,()=>{
+  console.log(`server start at localhost:${port}`);
+})
